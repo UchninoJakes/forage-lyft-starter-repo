d18493053b63700916bb87b3bb86d5e85a742c20
UchninoJakes <88137106+UchninoJakes@users.noreply.github.com>
2022-03-27 09:16:18 +0100

    Update sternman_engine.py

--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -1,15 +1,9 @@
-from abc import ABC
+from carfactory import CarFactory
+from engine import Engine
 
-from car import Car
 
+class SternmanEngine(CarFactory, Engine):
+    @property
+    def needs_service(self):
+        return self.needs_service()
 
-class SternmanEngine(Car, ABC):
-    def __init__(self, last_service_date, warning_light_is_on):
-        super().__init__(last_service_date)
-        self.warning_light_is_on = warning_light_is_on
-
-    def engine_should_be_serviced(self):
-        if self.warning_light_is_on:
-            return True
-        else:
-            return False
